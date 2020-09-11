5ec906e979f4103666ec965d37b3933449bfa8e9
rewe999 <zymek998@gmail.com>
2020-09-11 17:34:09 +0200

    new home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,19 +4,19 @@ import Article from '../components/Article'
 const articles = [
     {
         id:1,
-        title:"gitara",
+        title:"Use Numbers",
         author:"Jan Kowal",
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint sed, fuga tenetur quis explicabo? Quaerat, voluptatem omnis. Distinctio nulla labore eum odit ratione consectetur officiis temporibus voluptas ex? Libero.'
     },
     {
         id:2,
-        title:"Krowa",
+        title:"Change The Reader's Life",
         author:"Adam Kowal",
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint sed, fuga tenetur quis explicabo? Quaerat, voluptatem omnis. Distinctio nulla labore eum odit ratione consectetur officiis temporibus voluptas ex? Libero.'
     },
     {
         id:3,
-        title:"Muaaa",
+        title:"Make a Prediction",
         author:"Ania Kowal",
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi sint sed, fuga tenetur quis explicabo? Quaerat, voluptatem omnis. Distinctio nulla labore eum odit ratione consectetur officiis temporibus voluptas ex? Libero.'
     }
