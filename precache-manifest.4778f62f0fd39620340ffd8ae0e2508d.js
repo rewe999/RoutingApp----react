39412c1918d53008e4fbb2e70112a7ddce344267
rewe999 <zymek998@gmail.com>
2020-09-11 17:21:18 +0200

    Updates

---
+++ precache-manifest.4778f62f0fd39620340ffd8ae0e2508d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe99cd983ba5fceeccbd29a3ab8127f",
+    "url": "/RoutingApp----react/index.html"
+  },
+  {
+    "revision": "a534cdb1192a4116498d",
+    "url": "/RoutingApp----react/static/css/main.ab7a5c14.chunk.css"
+  },
+  {
+    "revision": "cb28d2966fb06dc2d060",
+    "url": "/RoutingApp----react/static/js/2.fcb02d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoutingApp----react/static/js/2.fcb02d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a534cdb1192a4116498d",
+    "url": "/RoutingApp----react/static/js/main.e34d2cb4.chunk.js"
+  },
+  {
+    "revision": "503da4ae2e6df7c3d2bf",
+    "url": "/RoutingApp----react/static/js/runtime-main.3ce4a940.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/RoutingApp----react/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/RoutingApp----react/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/RoutingApp----react/static/media/header3.d9d36b65.jpg"
+  }
+]);
